c50298229ba480d685825d1f865e19a1fbfcc175
kalp202 <p.kalp202@gmail.com>
2025-03-11 17:21:09 +0530

    Add middleware for alumni

--- Backend/src/Middlewares/auth.middleware.js
+++ Backend/src/Middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ import ApiError from "../Utils/ApiError.js";
 import asyncHandler from "../Utils/AsyncHandler.js";
 import jwt from "jsonwebtoken";
 import { Admin } from "../Models/admin.model.js";
+import { Alumni } from "../Models/alumni.model.js"
 
 const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
@@ -37,4 +38,18 @@ const verifyAdmin = asyncHandler(async (req, _, next) => {
     next()
 })
 
-export { verifyJWT, verifyAdmin }
+const verifyAlumni = asyncHandler(async (req, _, next) => {
+    const token = req?.headers?.Authorization.split(" ")[1]
+    if (!token)
+        throw new ApiError(404, "Unauthorized request")
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const alumni = await Alumni.findById(decodedToken._id).select(" -password -refreshToken")
+    if (!alumni)
+        throw new ApiError(401, "invalid access token")
+
+    req.alumni = alumni
+    next()
+})
+
+export { verifyJWT, verifyAdmin, verifyAlumni }
