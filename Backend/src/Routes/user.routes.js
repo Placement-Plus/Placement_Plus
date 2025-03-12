5995222b01540ca7092869c1f659d3ebbe605ab2
kalp202 <p.kalp202@gmail.com>
2025-03-12 16:53:48 +0530

    Update user routes

--- Backend/src/Routes/user.routes.js
+++ Backend/src/Routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../Middlewares/multer.middleware.js"
-import { changePassword, getCurrentUser, loginUser, logoutUser, registerUser, updateDeatils, uploadResume } from "../Controllers/user.controller.js";
+import { changePassword, getCurrentUser, loginUser, logoutUser, registerUser, updateDeatils, uploadResume, viewResume } from "../Controllers/user.controller.js";
 import { verifyJWT } from "../Middlewares/auth.middleware.js";
 
 const router = Router()
@@ -16,5 +16,6 @@ router.route("/get-user").get(verifyJWT, getCurrentUser)
 router.route("/change-password").patch(verifyJWT, changePassword)
 router.route("/upload-resume").patch(verifyJWT, upload.fields([{ name: "resume", maxCount: 1 }]), uploadResume)
 router.route("/update-details").patch(verifyJWT, updateDeatils)
+router.route("/view-resume").get(verifyJWT, viewResume)
 
 export default router
