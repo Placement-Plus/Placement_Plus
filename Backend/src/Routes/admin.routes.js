a7afc9f2413aac004edd4ea88c27037fae26c167
kalp202 <p.kalp202@gmail.com>
2025-03-12 16:52:22 +0530

    Changes in admin routes

--- Backend/src/Routes/admin.routes.js
+++ Backend/src/Routes/admin.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginAdmin, logoutAdmin, registerAdmin } from "../Controllers/admin.controller.js";
+import { exportToExcel, exportToPDF, loginAdmin, logoutAdmin, registerAdmin } from "../Controllers/admin.controller.js";
 import { verifyAdmin } from "../Middlewares/auth.middleware.js"
 
 const router = Router()
@@ -7,6 +7,9 @@ const router = Router()
 router.route("/register").post(registerAdmin)
 router.route("/login").post(loginAdmin)
 router.route("/logout").post(verifyAdmin, logoutAdmin)
+router.route("/export-to-pdf").get(verifyAdmin, exportToPDF)
+router.route("/export-to-excel").get(verifyAdmin, exportToExcel)
+
 
 
 export default router
