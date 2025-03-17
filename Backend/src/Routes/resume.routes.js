be8c470b99344ae24a2976471f20f448f45d87cc
kalp202 <p.kalp202@gmail.com>
2025-03-17 16:22:58 +0530

    Add resume routes

--- Backend/src/Routes/resume.routes.js
+++ Backend/src/Routes/resume.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../Middlewares/auth.middleware.js"
-import { getFinalResumeScore, getResumeSuggestion, parseResume } from "../Controllers/resumeController.js";
+import { getFinalResumeScore, getResumeSuggestion, parseResume } from "../Controllers/resume.controller.js";
 import { upload } from "../Middlewares/multer.middleware.js"
 
 
