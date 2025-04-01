0dc19000f4d6bb213f05229acd10cc4a17c465c5
kalp202 <p.kalp202@gmail.com>
2025-04-01 14:54:59 +0530

    Add context for admin

--- Frontend/context/userContext.js
+++ Frontend/context/userContext.js
@@ -5,6 +5,9 @@ const UserContext = createContext();
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [theme, setTheme] = useState("dark");
+    const [admin, setAdmin] = useState(null)
+    const [isAdminLoggesIn, setIsAdminLoggedIn] = useState(false)
 
     const login = (userData) => {
         setUser(userData);
@@ -16,8 +19,22 @@ export const UserProvider = ({ children }) => {
         setIsLoggedIn(false);
     };
 
+    const adminLogin = (adminData) => {
+        setAdmin(adminData)
+        setIsAdminLoggedIn(true)
+    }
+
+    const adminLogout = () => {
+        setAdmin(false)
+        setIsAdminLoggedIn(false)
+    }
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
     return (
-        <UserContext.Provider value={{ user, isLoggedIn, login, logout }}>
+        <UserContext.Provider value={{ user, isLoggedIn, admin, isAdminLoggesIn, login, logout, adminLogin, adminLogout, theme, toggleTheme }}>
             {children}
         </UserContext.Provider>
     );
